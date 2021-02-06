7aabe88e89252a65491ab4a2e5f8e5fcf2946514
Ahmed Awadallah <aawadall@ualberta.ca>
2021-02-05 19:13:28 -0500

    minor cleanup

---
+++ data/mongoDriver.js
@@ -0,0 +1,14 @@
+
+
+function getLogs(searchTerm, callback) {
+    // TODO    
+}
+
+function writeLogs(payload, callback) {
+    // TODO
+}
+
+module.exports = new {
+    getLogs: getLogs,
+    writeLogs: writeLogs
+}
--- services/logsService.js
+++ services/logsService.js
@@ -1,11 +1,26 @@
+const data = require('../data/mongoDriver')
+
 function getLogs(req, callback) {
     // TODO
+    data.getLogs(getSearchTerms(req, callback))
 }
 
 function postLogs(req, callback) {
     // TODO
+    data.writeLogs(getPayload(req, callback))
 }
 
+function getSearchTerms(req) {
+    // TODO 
+    let searchTerms = new {}
+    return searchTerms
+}
+
+function getPayload(req) {
+    // TODO
+    let payload = new {}
+    return payload
+}
 module.exports = {
     getLogs: getLogs,
     postLogs: postLogs
