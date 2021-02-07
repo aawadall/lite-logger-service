732a55281b7214957a7c57c5d7e6ca6d26d1eabf
Ahmed Awadallah <aawadall@ualberta.ca>
2021-02-06 21:42:16 -0500

    more in test

--- integration.sh
+++ integration.sh
@@ -2,9 +2,10 @@
 curl -X POST \
  -H "Content-Type: application/json" \
  -d @tests/sample_payload_1.json \
- http://logger-service:3000/api/logs
+ http://localhost:3000/api/logs
 
 # get entry 
 curl -X GET \
 -H "Content-Type: application/json" \
-    http://logger-service:3000/api/logs?id=test1 
+    http://localhost:3000/api/logs?id=test1 
+    
